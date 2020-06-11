3e948ce2928744cad6ebd3373e5a68adbf953989
Akshita Agarwal <akagarw@microsoft.com>
2020-06-10 19:14:30 -0700

    add the original one

--- TrafficLightFlow.py
+++ TrafficLightFlow.py
@@ -1,12 +1,10 @@
-"""Grid/green wave example."""
-
-import json
-
-from flow.utils.registry import make_create_env
+"""Traffic Light Grid example."""
 from flow.core.params import SumoParams, EnvParams, InitialConfig, NetParams, \
     InFlows, SumoCarFollowingParams
 from flow.core.params import VehicleParams
 from flow.controllers import SimCarFollowingController, GridRouter
+from flow.envs import TrafficLightGridPOEnv, TrafficLightGridEnv, AccelEnv
+from flow.networks import TrafficLightGridNetwork
 from flow.core.experiment import Experiment
 from StaticAgent import static_rl_actions
 from flow.utils.registry import make_create_env
@@ -22,21 +20,32 @@ HORIZON = 200
 # number of rollouts per training iteration
 N_ROLLOUTS = 30
 # number of parallel workers
-#N_CPUS = 2
-N_CPUS = 1
+N_CPUS = 2
+# set to True if you would like to run the experiment with inflows of vehicles
+# from the edges, and False otherwise
+USE_INFLOWS = True
 
-
-def gen_edges(row_num, col_num):
+def gen_edges(col_num, row_num):
+    """Generate the names of the outer edges in the traffic light grid network.
+    Parameters
+    ----------
+    col_num : int
+        number of columns in the traffic light grid
+    row_num : int
+        number of rows in the traffic light grid
+    Returns
+    -------
+    list of str
+        names of all the outer edges
+    """
     edges = []
     for i in range(col_num):
         edges += ['left' + str(row_num) + '_' + str(i)]
         edges += ['right' + '0' + '_' + str(i)]
-
     # build the left and then the right edges
     for i in range(row_num):
         edges += ['bot' + str(i) + '_' + '0']
         edges += ['top' + str(i) + '_' + str(col_num)]
-
     return edges
 def get_inflow_params(col_num, row_num, additional_net_params, inflow_probability):
     """Define the network and initial params in the presence of inflows.
@@ -68,12 +77,6 @@ def get_inflow_params(col_num, row_num, additional_net_params, inflow_probabilit
     #         return 0.05
     #     return 0.15
 
-def get_flow_params(col_num, row_num, additional_net_params):
-    initial_config = InitialConfig(
-        spacing='custom', lanes_distribution=float('inf'), shuffle=True)
-
-    inflow = InFlows()
-    outer_edges = gen_edges(col_num, row_num)
     for i in range(len(outer_edges)):
         inflow.add(
             veh_type='idm',
@@ -84,9 +87,7 @@ def get_flow_params(col_num, row_num, additional_net_params):
     net = NetParams(
         inflows=inflow,
         additional_params=additional_net_params)
-
-    return initial_config, net_params
-
+    return initial, net
 
 def get_non_flow_params(enter_speed, add_net_params):
     """Define the network and initial params in the absence of inflows.
@@ -100,7 +101,7 @@ def get_non_flow_params(enter_speed, add_net_params):
     enter_speed : float
         initial speed of vehicles as they enter the network.
     add_net_params: dict
-        additional network-specific parameters (unique to the grid)
+        additional network-specific parameters (unique to the traffic light grid)
 
     Returns
     -------
@@ -108,30 +109,27 @@ def get_non_flow_params(enter_speed, add_net_params):
         parameters specifying the initial configuration of vehicles in the
         network
     flow.core.params.NetParams
-        network-specific parameters used to generate the scenario
+        network-specific parameters used to generate the network
     """
     additional_init_params = {'enter_speed': enter_speed}
-    initial_config = InitialConfig(
+    initial = InitialConfig(
         spacing='custom', additional_params=additional_init_params)
-    net_params = NetParams(additional_params=add_net_params)
+    net = NetParams(additional_params=add_net_params)
 
-    return initial_config, net_params
+    return initial, net
 
 
-V_ENTER = 30
+
+V_ENTER = 10
 INNER_LENGTH = 300
 LONG_LENGTH = 100
 SHORT_LENGTH = 300
 N_ROWS = 1
 N_COLUMNS = 1
-# NUM_CARS_LEFT = 1
-# NUM_CARS_RIGHT = 1
-# NUM_CARS_TOP = 1
-# NUM_CARS_BOT = 1
-NUM_CARS_LEFT = 10
-NUM_CARS_RIGHT = 10
-NUM_CARS_TOP = 10
-NUM_CARS_BOT = 10
+NUM_CARS_LEFT = 1
+NUM_CARS_RIGHT = 1
+NUM_CARS_TOP = 1
+NUM_CARS_BOT = 1
 tot_cars = (NUM_CARS_LEFT + NUM_CARS_RIGHT) * N_COLUMNS \
            + (NUM_CARS_BOT + NUM_CARS_TOP) * N_ROWS
 
@@ -146,7 +144,7 @@ grid_array = {
     "cars_top": NUM_CARS_TOP,
     "cars_bot": NUM_CARS_BOT
 }
-
+ 
 additional_env_params = {
         'target_velocity': 50,
         'switch_time': 4.0,
@@ -154,24 +152,25 @@ additional_env_params = {
         'discrete': True,
         'tl_type': 'controlled'
     }
-
-
+ 
 additional_net_params = {
     'speed_limit': 35,
     'grid_array': grid_array,
-    'horizontal_lanes': 2,
-    'vertical_lanes': 2,
-    'traffic_lights': True ######### !
+    'horizontal_lanes': 1,
+    'vertical_lanes': 1
 }
-
+ 
 vehicles = VehicleParams()
-vehicles.add( 
-    veh_id='idm', 
+vehicles.add(
+    veh_id='idm',
     acceleration_controller=(SimCarFollowingController, {}),
-    car_following_params=SumoCarFollowingParams( min_gap=2.5, max_speed=V_ENTER, speed_mode="all_checks",tau=1.1 ), 
-    lane_change_params=SumoLaneChangeParams( lane_change_mode="strategic", model="LC2013", ), 
-    lane_change_controller=(StaticLaneChanger, {}), 
-    routing_controller=(MinicityRouter, {}),
+    car_following_params=SumoCarFollowingParams(
+        min_gap=2.5,
+        decel=7.5,  # avoid collisions at emergency stops
+        max_speed=V_ENTER,
+        speed_mode="all_checks",
+    ),
+    routing_controller=(GridRouter, {}),
     num_vehicles=tot_cars)
  
 # collect the initialization and network-specific parameters based on the
